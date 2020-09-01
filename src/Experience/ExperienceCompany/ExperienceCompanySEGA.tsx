086f4fd1964811044d4190c7fa7ed1221256687e
Anders Lundback <anders.lundback@postoffice.co.uk>
2020-09-01 12:31:58 +0100

    Text refinement

--- src/Experience/ExperienceCompany/ExperienceCompanySEGA.tsx
+++ src/Experience/ExperienceCompany/ExperienceCompanySEGA.tsx
@@ -11,7 +11,7 @@ export default function ExperienceCompanySEGA() {
             position="Localisation Quality Tester"
             period="Jan 2016 - Apr 2016"
             stack="Bug tracking & reporting, regression, translation
-            Atlassian Suite, TechExcel DevSuite, MS Office"
+            Atlassian Suite, TechExcel DevSuite, MS Office."
             details={
                 <>
                     <p>
@@ -24,14 +24,10 @@ export default function ExperienceCompanySEGA() {
                             SEGA
                         </a>
                         , one of the leading interactive entertainment companies
-                        in the world, with it's European HQ based in London
+                        in the world, with it's European HQ based in London.
                     </p>
                     <p>
-                        Quality Assurance and localisation responsible for the
-                        Swedish language in the market leading video game
-                        Football Manager. Bug tracking and reporting,
-                        regression, translation with a multilingual team of
-                        testers.
+                        Quality Assurance and localisation responsible for the Swedish language in the market leading video game Football Manager. Bug tracking and reporting, regression, translation with a multilingual team of testers.
                     </p>
                 </>
             }
