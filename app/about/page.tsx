68cf2e5eadb919c7593c37805a12d52d1bb93087
Anders Lundbäck <lundback.anders@gmail.com>
2024-09-24 10:49:04 +0200

    chore: add more services info

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ const AboutMe: React.FC = () => (
     <Typography variant="h1">About me</Typography>
     <Typography paragraph>
       {`Product team leader with a decade of commercial engineering experience.
-      I enjoy putting a team together, empowering leaders and either finding
+      I enjoy putting a team together, empowering   leaders and either finding
       the product-market-fit, or scaling up, depending on the organisation. I
       have worked in various company sizes, and industries across Europe.`}
     </Typography>
@@ -22,11 +22,20 @@ const AboutMe: React.FC = () => (
         padel
       </Link>
       {` and chess. I've been playing the latter competitively since
-      I was a kid. If you're interested in chess, check out my website `}
-      <Link href="https://better-chess.com/" target="_blank" rel="noopener">
-        Better Chess
+      childhood.`}
+    </Typography>
+    <Typography paragraph>
+      {`I’m available for freelance consulting to help your business thrive. Whether you're looking to find your product-market fit, optimize your website for search engines, or need support with web and app development, I can provide the strategic guidance and hands-on expertise you need.`}
+    </Typography>
+    <Typography paragraph>
+      {`Lundback Product Consulting AB. Registered limited company in Sweden. Organisation number: 559495-3738.`}
+    </Typography>
+    <Typography paragraph>
+      {`Learn more about `}
+      <Link href="/services" target="_blank" rel="noopener">
+        my services
       </Link>
-      .
+      {` and how I can help your business.`}
     </Typography>
   </div>
 );
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -4,12 +4,38 @@ import React from "react";
 
 const Services: React.FC = () => (
   <div className="is-text" data-testid="Services">
-    <Typography variant="h1">Services</Typography>
-    <Typography paragraph>
-      {`I am open for freelance work. Contact me if you need help with your business. I can help you find your product-market fit, optimise your site for search engines and support with web and app development.`}
+    <Typography variant="h4" component="h1">
+      Services
     </Typography>
     <Typography paragraph>
-      {`Lundback Product Consulting AB. Registered limited company in Sweden with organisation number: 559495-3738.`}
+      {`I’m available for freelance consulting to help your business thrive. Whether you're looking to find your product-market fit, optimize your website for search engines, or need support with web and app development, I can provide the strategic guidance and hands-on expertise you need.`}
+    </Typography>
+    <Typography paragraph>
+      {`Lundback Product Consulting AB. Registered limited company in Sweden. Organisation number: 559495-3738.`}
+    </Typography>
+    <Typography variant="h5" component="h2">
+      What I Offer:
+    </Typography>
+    <Typography paragraph>
+      {`• Strategic support during the product discovery phase, using design thinking to align your product vision with business goals.`}
+    </Typography>
+    <Typography paragraph>
+      {`• Leading story mapping, storyboarding, release planning, and roadmap sessions to set a clear and actionable direction.`}
+    </Typography>
+    <Typography paragraph>
+      {`• Building and managing a product backlog, writing user stories and acceptance criteria to ensure a smooth development process.`}
+    </Typography>
+    <Typography paragraph>
+      {`• Facilitating backlog refinement meetings, prioritizing tasks, and planning sprints to keep development on track.`}
+    </Typography>
+    <Typography paragraph>
+      {`• Collaborating with design teams to create user-focused UI/UX that aligns with your overall product strategy.`}
+    </Typography>
+    <Typography paragraph>
+      {`• Conducting Sprint Reviews to demo progress, gather stakeholder feedback, and adjust as needed.`}
+    </Typography>
+    <Typography paragraph>
+      {`• Running customer research, including surveys and interviews, to identify key metrics and improve your product’s impact.`}
     </Typography>
   </div>
 );
